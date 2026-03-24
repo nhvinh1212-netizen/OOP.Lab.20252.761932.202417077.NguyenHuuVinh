913c008d22d285db94345e1f4e84a81b2f0a9b46
nhvinh1212-netizen <nhvinh1212@gmail.com>
2026-03-24 17:03:37 +0700

    a

--- Lab 01/FirstDialog.java
+++ Lab 01/FirstDialog.java
@@ -1,6 +1,5 @@
 
-
-import java.swing.JOptionPane;
+import javax.swing.JOptionPane;
 public class FirstDialog {
 	public static void main(String[] args) {
 		JOptionPane.showMessageDialog(null, "Hello World! How are you");
